ec7bec13c0722006ff2c9519bb591015564fa340
rpdict <rpdict@gmail.com>
2016-11-20 12:42:52 +0800

    修复了导航条高亮

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,2 +1,9 @@
-import "node_modules/bootstrap/dist/js/bootstrap";
-import "node_modules/jquery/dist/jquery";
+$("document").ready(function(){
+  if (location.pathname === '/') {
+    $("nav a:eq(0)").addClass("active");
+  } else if (location.pathname === '/markdown') {
+    $("nav a:eq(1)").addClass("active");
+  } else {
+    $("nav a").removeClass("active");
+  }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,15 @@ var gulp = require('gulp');
 gulp.task('frontend-styles', function() {
   gulp.src('frontend/styles/main.scss') //该任务针对的文件
           .pipe(less()) //该任务调用的模块
-          .pipe(gulp.dest('public/stylesheets/')); //将会在src/css下生成index.css
+          .pipe(gulp.dest('public/stylesheets/')); //将会在public/stylesheets下生成main.css
 });
 
 gulp.task('frontend-scripts', function() {
-  return gulp.src(['node_modules/jquery/dist/jquery.js'],['node_modules/bootstrap/dist/js/bootstrap.js'])
+  return gulp.src(['node_modules/jquery/dist/jquery.js',
+                  'node_modules/bootstrap/dist/js/bootstrap.js',
+                  'frontend/scripts/main.js'])
           .pipe(concat('vendor.js')) //该任务调用的模块
-          .pipe(gulp.dest('public/javascripts/')); //将会在src/css下生成index.css
+          .pipe(gulp.dest('public/javascripts/')); //将会在public/javascripts下生成vendor.js
 });
 
 gulp.task('default', ['frontend-styles', 'frontend-scripts']);
